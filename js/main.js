33eae8d0b7eed1c16e2edfc2ba3bed598d8fbb53
Matthew Vernon <hello@matthewvernon.co>
2018-06-01 19:56:25 +1000

    added in wavy line, fixed up headers on mobile

--- js/main.js
+++ js/main.js
@@ -76,3 +76,34 @@ document.querySelector('.next').addEventListener('click', () => mySiema.next());
       }
     }
   })
+
+  // wave ====
+
+let xs = []
+for (var i = 0; i <= 500; i++) {
+  xs.push(i)
+}
+
+let t = 0
+
+function animate() {
+
+  let points = xs.map(x => {
+
+    let y = 25 + 10 * Math.sin((x + t) / 10)
+
+    return [x, y]
+  })
+
+  let path = "M" + points.map(p => {
+    return p[0] + "," + p[1]
+  }).join(" L")
+
+  document.querySelector("path").setAttribute("d", path)
+
+  t += 0.5
+
+  requestAnimationFrame(animate)
+}
+
+animate()
